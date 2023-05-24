2b2a4ebdb9b334fb4508331f122c1d82e78f788b
Henry-Kofi <hnyonyo001@st.ug.edu.gh>
2023-05-24 03:03:52 +0000

    hi

---
+++ script.js
@@ -0,0 +1,12 @@
+const textAreaQuestionEl = document.querySelector('.text_area_container');
+const keyContainerEl= document.querySelector('key_container');
+const btnEl = document.querySelector('button')
+
+// let  text = textAreaQuestion.textContent;
+
+let textEl = textAreaQuestionEl.value;
+
+keyContainerEl.addEventListener('input', () => {
+    console.log('1')
+})
+
